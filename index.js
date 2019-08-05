4ffae6347f9864751f7a6481b56ba979fd518b97
Maarten Blömer <lucmaar@hotmail.com>
2019-08-05 14:11:16 +0200

    Reverted server.post

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
 });
 
 // Listen for incoming activities and route them to your bot main dialog.
-server.post('cy2-chatbot1.azurewebsites.net/api/messages', (req, res) => {
+server.post('/api/messages', (req, res) => {
     // Route received a request to adapter for processing
     adapter.processActivity(req, res, async (turnContext) => {
         // route to bot activity handler.
